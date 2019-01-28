184327f06f221ab6f219f8a35523ed235c5402ff
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2019-01-28 09:42:01 +0800

    paging component previous bug fix ing

--- src/components/PagingComponent.js
+++ src/components/PagingComponent.js
@@ -1,18 +1,39 @@
 import React, { Fragment } from "react";
 import { Button, Icon } from "antd";
-const PagingComponent = ({meta,onPrevClick,onNextClick,loading}) => {
-  
+import { withState } from "recompose";
+
+const PagingComponent = ({
+  meta,
+  onPrevClick,
+  onNextClick,
+  loading,
+  totalPage,
+  setTotalPage
+}) => {
   return (
     <Button.Group style={{ float: `right` }} size={"large"}>
-      {meta.offset>0 && <Button type="primary" onClick={()=>onPrevClick(meta.offset-1)} loading={loading}>
-        <Icon type="left" />
-        Prev
-      </Button>
-      }
-      {meta.offset<Math.ceil(meta.total/meta.limit)&& <Button type="primary" onClick={()=>onNextClick(meta.offset+1)} loading={loading}>
-        Next
-        <Icon type="right" />
-      </Button>}
+      {meta.offset > 0 && (
+        <Button
+          type="primary"
+          onClick={() => {
+            onPrevClick(meta.offset - 18);
+          }}
+          loading={loading}
+        >
+          <Icon type="left" />
+          Prev
+        </Button>
+      )}
+      {meta.offset-18 < Math.ceil(meta.total) && (
+        <Button
+          type="primary"
+          onClick={() => onNextClick(meta.offset + 18)}
+          loading={loading}
+        >
+          Next
+          <Icon type="right" />
+        </Button>
+      )}
     </Button.Group>
   );
 };
